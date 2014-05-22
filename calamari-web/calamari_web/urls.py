a03b6d6e737ca0ef32ce34ac4275253cd3d7a4c6
John Spray <john.spray@inktank.com>
2014-05-22 17:28:45 +0100

    calamari_web: drop graphite urls if absent
    Signed-off-by: John Spray <john.spray@inktank.com>

--- calamari-web/calamari_web/urls.py
+++ calamari-web/calamari_web/urls.py
@@ -6,7 +6,6 @@ from settings import STATIC_ROOT, GRAPHITE_API_PREFIX, CONTENT_DIR
 # from django.contrib import admin
 # admin.autodiscover()
 
-
 urlpatterns = patterns(
     '',
 
@@ -20,44 +19,34 @@ urlpatterns = patterns(
     url(r'^api/v1/', include('calamari_rest.urls.v1')),
     url(r'^api/v2/', include('calamari_rest.urls.v2')),
 
+    url(r'^admin/(?P<path>.*)$', 'calamari_web.views.serve_dir_or_index',
+        {'document_root': '%s/admin/' % STATIC_ROOT}),
+
+    url(r'^login/$', 'django.views.static.serve',
+        {'document_root': '%s/login/' % STATIC_ROOT, 'path': "index.html"}),
+
+    url(r'^login/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': '%s/login/' % STATIC_ROOT}),
+
     url(r'^bootstrap$', 'calamari_web.views.bootstrap', name='bootstrap'),
 
     url(r'^dashboard/(?P<path>.*)$', 'calamari_web.views.dashboard',
         {'document_root': '%s/dashboard/' % STATIC_ROOT},
         name='dashboard'),
 
-    url(r'^render/?', include('graphite.render.urls')),
-    url(r'^metrics/?', include('graphite.metrics.urls')),
-    url(r'^%s/dashboard/?' % GRAPHITE_API_PREFIX.lstrip('/'), include('graphite.dashboard.urls')),
-
-    # XXX this is a hack to make graphite visible where the 1.x GUI expects it,
-    url(r'^graphite/render/?', include('graphite.render.urls')),
-    url(r'^graphite/metrics/?', include('graphite.metrics.urls')),
+    url(r'^manage/(?P<path>.*)$', 'calamari_web.views.serve_dir_or_index',
+        {'document_root': '%s/manage/' % STATIC_ROOT}),
 
     # XXX this is a hack to make graphite dashboard work in dev mode (full installation
     # serves this part with apache)
     url('^content/(?P<path>.*)$', 'django.views.static.serve', {'document_root': CONTENT_DIR}),
 
     # XXX this is a hack to serve apt repo in dev mode (Full installation serves this with apache)
-    url(r'^static/precise/(?P<path>.*)$', 'django.views.static.serve',
-        {'document_root': '%s/precise/' % STATIC_ROOT}),
-    url(r'^static/trusty/(?P<path>.*)$', 'django.views.static.serve',
-        {'document_root': '%s/trusty/' % STATIC_ROOT}),
-    url(r'^static/wheezy/(?P<path>.*)$', 'django.views.static.serve',
-        {'document_root': '%s/wheezy/' % STATIC_ROOT}),
+    url(r'^static/ubuntu/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': '%s/ubuntu/' % STATIC_ROOT}),
     url(r'^static/el6/(?P<path>.*)$', 'django.views.static.serve',
         {'document_root': '%s/el6/' % STATIC_ROOT}),
-    url(r'^static/rhel6/(?P<path>.*)$', 'django.views.static.serve',
-        {'document_root': '%s/rhel6/' % STATIC_ROOT}),
-    url(r'^static/rhel7/(?P<path>.*)$', 'django.views.static.serve',
-        {'document_root': '%s/rhel7/' % STATIC_ROOT}),
 )
-
-UI_PATHS = ['login', 'admin', 'manage']
-
-for path in UI_PATHS:
-    urlpatterns = urlpatterns + [url(r'^{0}/(?P<path>.*)$'.format(path), 'calamari_web.views.serve_dir_or_index',
-                                     {'document_root': '{0}/{1}/'.format(STATIC_ROOT, path)})]
 
 handler500 = 'calamari_web.views.server_error'
 
@@ -83,7 +72,21 @@ import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning,
                         message="django.conf.urls.defaults is deprecated")
 
-import graphite.metrics.urls
-import graphite.dashboard.urls
-patch_views(graphite.metrics.urls)
-patch_views(graphite.dashboard.urls)
+try:
+    import graphite.metrics.urls
+    import graphite.dashboard.urls
+except ImportError:
+    pass
+else:
+    urlpatterns.extend([
+        url(r'^render/?', include('graphite.render.urls')),
+        url(r'^metrics/?', include('graphite.metrics.urls')),
+        url(r'^%s/dashboard/?' % GRAPHITE_API_PREFIX.lstrip('/'), include('graphite.dashboard.urls')),
+
+        # XXX this is a hack to make graphite visible where the 1.x GUI expects it,
+        url(r'^graphite/render/?', include('graphite.render.urls')),
+        url(r'^graphite/metrics/?', include('graphite.metrics.urls')),
+    ])
+
+    patch_views(graphite.metrics.urls)
+    patch_views(graphite.dashboard.urls)
