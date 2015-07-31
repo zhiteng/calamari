d6e4b4076d1cc8cec7fced02a746736b1ec0b9e7
Gregory Meno <gmeno@redhat.com>
2015-07-31 22:21:24 +0000

    devmode: we installed salt already when we provisioned vagrant
    Signed-off-by: Gregory Meno <gmeno@redhat.com>

--- vps_bootstrap.sh
+++ vps_bootstrap.sh
@@ -1,14 +1,5 @@
 #/bin/bash
 set -e
 
-if [ -x $(which wget) ]
-then
-    wget -O install_salt.sh https://bootstrap.saltstack.com
-else
-    curl -o install_salt.sh -L https://bootstrap.saltstack.com
-fi
-
-sudo sh install_salt.sh -G -P git v2014.7.0
-
 pillar_data="{"\"username\":\"$USER\""}"
 sudo salt-call --local --file-root=$(pwd)/vagrant/devmode/salt/roots state.highstate pillar="$pillar_data"
